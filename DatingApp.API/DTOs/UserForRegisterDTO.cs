69fbcf31f1498eb7062ad89cd6896412012bb1dd
Domenico Maisano <dmaisano@snowjoe.com>
2020-01-21 15:17:56 -0500

    wip

--- DatingApp.API/DTOs/UserForRegisterDTO.cs
+++ DatingApp.API/DTOs/UserForRegisterDTO.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTOs
 {
     public class UserForRegisterDTO
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(32, MinimumLength = 8, ErrorMessage = "You must provide a password between {2} and {1} characters")]
         public string Password { get; set; }
     }
 }
